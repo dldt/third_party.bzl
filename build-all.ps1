935313e85d822ef5f8ddd8985faa6c2dbf24e2b1
Thomas Arcila <thomas.arcila@gmail.com>
2021-01-06 15:00:06 +0100

    github: Extract build script so builds can be tested locally

---
+++ build-all.ps1
@@ -0,0 +1,9 @@
+[String[]]${PACKAGES} = & bazel query ...:* --output=package | ? { -not [String]::IsNullOrEmpty($_) }
+${QUERY_SPEC} = (${PACKAGES} | % { "@${_}//:all" }) -Join " union "
+[String[]]${TARGETS} = & bazel query "visible(...:*, ${QUERY_SPEC})"
+& bazel build --keep_going @TARGETS
+if ($LastExitCode -ne "0") {
+    Write-Host "The following failed to build:"
+    & bazel build --check_up_to_date --show_result 0 @TARGETS
+    exit 1
+}
